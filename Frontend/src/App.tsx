1c11ff349bc8b804a494eade8f3accd7d8f061d3
Sparsh Gupta <sparshgupta78970@gmail.com>
2026-03-20 14:34:16 +0530

    Add frontend properly

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      <Button>
+        Hello world
+      </Button>
+    </div>
+  )
+}
+
+export default App
