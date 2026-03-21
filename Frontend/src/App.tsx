2c90ba71d07320e5e6d4d03b0013ebdebdf6409d
AnantSrivastava-git <anantsrivastava65@gmail.com>
2026-03-21 13:22:40 +0530

    Added Signup/Login routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,10 +3,14 @@ import Pathway from "./components/Pathway/Pathway";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Home from "./components/Home/Home";
 import ModulesDocs from "./components/Module/ModuleDocs";
+import Signup from "./components/Signup/Signup";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/" element={<Home />} />
       <Route path="/pathway" element={<Pathway />} />
       <Route path="/dashboard" element={<Dashboard />} />
